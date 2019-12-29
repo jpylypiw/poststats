3a3b5ad4e4c78b0e22c1e28d4fe591e13d4e5227
Jochen Pylypiw <jochen.pylypiw@noris.de>
2019-12-29 11:16:05 +0100

    developed new version 1.1.0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# poststats [![Codacy Badge](https://api.codacy.com/project/badge/Grade/9f54350a80c74d45bce11633aa50629d)](https://www.codacy.com/app/KingJP/poststats?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=KingJP/poststats&amp;utm_campaign=Badge_Grade) [![version](https://img.shields.io/badge/version-1.0.0-green.svg)](https://github.com/KingJP/poststats)
+# poststats [![Codacy Badge](https://api.codacy.com/project/badge/Grade/9f54350a80c74d45bce11633aa50629d)](https://www.codacy.com/app/jpylypiw/poststats?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=jpylypiw/poststats&amp;utm_campaign=Badge_Grade) [![version](https://img.shields.io/badge/version-1.1.0-green.svg)](https://github.com/jpylypiw/poststats)
 
 Nearly every **Linux Server** owner is running his own **Postfix Mailserver**. Securely sending emails becomes more and more complicated over time. **Statistics** on the Postfix Mailserver can currently only be accessed via **Mailgraph**. We provide an alternative presentation by **e-mail**. The software is developed in **Bash** for performance reasons and is very easy to install. We are looking forward to your feedback!
 
@@ -11,7 +11,7 @@ Nearly every **Linux Server** owner is running his own **Postfix Mailserver**. S
 
 ## Install
 
-1. Clone repository using `git clone https://github.com/KingJP/poststats.git` or download whole repository into a previously created folder using `wget https://github.com/KingJP/poststats/archive/master.zip && unzip master.zip`
+1. Clone repository using `git clone https://github.com/jpylypiw/poststats.git` or download whole repository into a previously created folder using `wget https://github.com/jpylypiw/poststats/archive/master.zip && unzip master.zip`
 1. Copy configuration Example to configuration file using `cp poststats.cfg.example poststats.cfg`
 1. Edit configuration file. You can find instructions in configuration file.
 1. Create Crontab for your favorite user. The user needs read permission on the logfiles provided in configuration file. A sample crontab could look like:
@@ -24,13 +24,30 @@ Nearly every **Linux Server** owner is running his own **Postfix Mailserver**. S
 
 poststats is licensed under the MIT License.
 
-It is not using any 3rd Party Tools. Please check its [License](https://github.com/kingjp/poststats/blob/master/LICENSE).
+It is not using any 3rd Party Tools. Please check its [License](https://github.com/jpylypiw/poststats/blob/master/LICENSE).
 
 ## Screenshots
 
 ![Screenshot 1: Responsive HTML E-Mail with Statistics](https://i.imgur.com/1IjtosB.png)
 
 ## Changelog
+
+### 1.1.0 (2019-12-29)
+
+Features:
+
+- Changed from MIT to GPL-3 license
+- checked the shellscript using shellcheck
+- moved the html template from a shell file to a html file for a improved editing experience
+- changed configuration example:
+  - added first rotated file to file check list (got a empty email on sunday because the logfile was already rotated)
+  - added a space between name and email address (rspamd complained about R_NO_SPACE_IN_FROM)
+- added rspamd logfile and count of action types
+
+Bugfixes:
+
+- IP address lookup was only searching for interface eth0 and is searching for all interfaces now
+- using grep for process searching was very ineffective. I switched to pgrep which brings a better result
 
 ### 1.0.0 (2017-12-16)
 
--- poststats.sh
+++ poststats.sh
@@ -2,19 +2,20 @@
 
 # __          _______  _  __
 # \ \        / /  __ \| |/ /
-#  \ \  /\  / /| |  | | ' / 
-#   \ \/  \/ / | |  | |  <  
-#    \  /\  /  | |__| | . \ 
+#  \ \  /\  / /| |  | | ' /
+#   \ \/  \/ / | |  | |  <
+#    \  /\  /  | |__| | . \
 #     \/  \/   |_____/|_|\_\
 #
-# Copyright (c) 2017 Webdesign Kronberg.
+
+# Copyright (c) 2019 Webdesign Kronberg.
 # ----------------------------------------------------------------------------
 # This file is analyzing log files for E-Mail Statistics.
 # DO NOT MAKE ANY CHANGES HERE! See poststats.cfg for configuration.
 # ----------------------------------------------------------------------------
 
 # Basic Variables and Configuration
-SCRIPTPATH=$(dirname $0)
+SCRIPTPATH=$(dirname "$0")
 LOGDATE=$(date -d "yesterday" +"%b %e")
 IPV4ADDRESS=$(/sbin/ifconfig | grep 'inet ' | awk '{ print $2}')
 IPV6ADDRESS=$(/sbin/ifconfig | grep '.*inet6 .*global.*' | awk '{ print $2}')
@@ -48,58 +49,73 @@ NOPTRCOUNT=0
 # BLACKHOLE
 BLACKHOLEDMAILCOUNT=0
 
+# OUTGOING
+SENTMAILCOUNT=0
+BOUNCEDMAILCOUNT=0
+
 # SPAMASSASSIN
 TESTEDMAILCOUNT=0
 SPAMCOUNT=0
 HAMCOUNT=0
 
-# OUTGOING
-SENTMAILCOUNT=0
-BOUNCEDMAILCOUNT=0
+# RSPAMD
+RSPAMD_REJECTED=0
+RSPAMD_ADD_HEADER=0
+RSPAMD_GREYLIST=0
+RSPAMD_NO_ACTION=0
+RSPAMD_SOFT_REJECT=0
+RSPAMD_REWRITE_SUBJECT=0
 
-function analyzeFile {
+function analyzeFile() {
     # CLAMAV
-    let CLEANCOUNT=$CLEANCOUNT+$(egrep -nc ".*$LOGDATE.*Clean message.*" $1)
-    let INFECTEDCOUNT=$INFECTEDCOUNT+$(egrep -nc ".*$LOGDATE.*infected by.*" $1)
+    let CLEANCOUNT=$CLEANCOUNT+$(grep -Enc ".*$LOGDATE.*Clean message from.*" "$1")
+    let INFECTEDCOUNT=$INFECTEDCOUNT+$(grep -Enc ".*$LOGDATE.*infected by.*" "$1")
 
     # IMAP
-    let IMAPLOGINCOUNT=$IMAPLOGINCOUNT+$(egrep -nc ".*$LOGDATE.*imap-login.*" $1)
-    let IMAPLOGINFAILCOUNT=$IMAPLOGINFAILCOUNT+$(egrep -nc ".*$LOGDATE.*imap-login: Disconnected.*" $1)
-    let IMAPIPV4LOGINCOUNT=$IMAPIPV4LOGINCOUNT+$(egrep -nc ".*$LOGDATE.*imap-login.*lip=[1-9]{1,3}\..*" $1)
-    let IMAPIPV6LOGINCOUNT=$IMAPIPV6LOGINCOUNT+$(egrep -nc ".*$LOGDATE.*imap-login.*lip=[0-9a-z]{0,4}\:.*" $1)
-    let IMAPWEBLOGINCOUNT=$IMAPWEBLOGINCOUNT+$(egrep -nc ".*$LOGDATE.*imap-login.*method=CRAM-MD5.*" $1)
+    let IMAPLOGINCOUNT=$IMAPLOGINCOUNT+$(grep -Enc ".*$LOGDATE.*imap-login.*" "$1")
+    let IMAPLOGINFAILCOUNT=$IMAPLOGINFAILCOUNT+$(grep -Enc ".*$LOGDATE.*imap-login: Disconnected.*" "$1")
+    let IMAPIPV4LOGINCOUNT=$IMAPIPV4LOGINCOUNT+$(grep -Enc ".*$LOGDATE.*imap-login.*lip=[1-9]{1,3}\..*" "$1")
+    let IMAPIPV6LOGINCOUNT=$IMAPIPV6LOGINCOUNT+$(grep -Enc ".*$LOGDATE.*imap-login.*lip=[0-9a-z]{0,4}\:.*" "$1")
+    let IMAPWEBLOGINCOUNT=$IMAPWEBLOGINCOUNT+$(grep -Enc ".*$LOGDATE.*imap-login.*method=CRAM-MD5.*" "$1")
 
     # POP3
-    let POP3LOGINCOUNT=$POP3LOGINCOUNT+$(egrep -nc ".*$LOGDATE.*pop3-login.*" $1)
-    let POP3LOGINFAILCOUNT=$POP3LOGINFAILCOUNT+$(egrep -nc ".*$LOGDATE.*pop3-login: Disconnected.*" $1)
-    let POP3IPV4LOGINCOUNT=$POP3IPV4LOGINCOUNT+$(egrep -nc ".*$LOGDATE.*pop3-login.*lip=[1-9]{1,3}\..*" $1)
-    let POP3IPV6LOGINCOUNT=$POP3IPV6LOGINCOUNT+$(egrep -nc ".*$LOGDATE.*pop3-login.*lip=[0-9a-z]{0,4}\:.*" $1)
-    let POP3WEBLOGINCOUNT=$POP3WEBLOGINCOUNT+$(egrep -nc ".*$LOGDATE.*pop3-login.*method=CRAM-MD5.*" $1)
+    let POP3LOGINCOUNT=$POP3LOGINCOUNT+$(grep -Enc ".*$LOGDATE.*pop3-login.*" "$1")
+    let POP3LOGINFAILCOUNT=$POP3LOGINFAILCOUNT+$(grep -Enc ".*$LOGDATE.*pop3-login: Disconnected.*" "$1")
+    let POP3IPV4LOGINCOUNT=$POP3IPV4LOGINCOUNT+$(grep -Enc ".*$LOGDATE.*pop3-login.*lip=[1-9]{1,3}\..*" "$1")
+    let POP3IPV6LOGINCOUNT=$POP3IPV6LOGINCOUNT+$(grep -Enc ".*$LOGDATE.*pop3-login.*lip=[0-9a-z]{0,4}\:.*" "$1")
+    let POP3WEBLOGINCOUNT=$POP3WEBLOGINCOUNT+$(grep -Enc ".*$LOGDATE.*pop3-login.*method=CRAM-MD5.*" "$1")
 
     # CONNECTION
-    let CONNECTIONCOUNT=$CONNECTIONCOUNT+$(egrep -nc ".*$LOGDATE.*connect from.*" $1)
-    let CONNECTIONIPV4COUNT=$CONNECTIONIPV4COUNT+$(egrep -nc ".*$LOGDATE.*connect from.*\[[1-9]{1,3}\..*" $1)
-    let CONNECTIONIPV6COUNT=$CONNECTIONIPV6COUNT+$(egrep -nc ".*$LOGDATE.*connect from.*\[[0-9a-z]{0,4}\:.*" $1)
-    let RELAYDENIED=$RELAYDENIED+$(egrep -nc ".*$LOGDATE.*NOQUEUE: reject.*Relay access denied.*" $1)
-    let NOPTRCOUNT=$NOPTRCOUNT+$(egrep -nc ".*$LOGDATE.*NOQUEUE: reject.*cannot find your reverse hostname.*" $1)
+    let CONNECTIONCOUNT=$CONNECTIONCOUNT+$(grep -Enc ".*$LOGDATE.*connect from.*" "$1")
+    let CONNECTIONIPV4COUNT=$CONNECTIONIPV4COUNT+$(grep -Enc ".*$LOGDATE.*connect from.*\[[1-9]{1,3}\..*" "$1")
+    let CONNECTIONIPV6COUNT=$CONNECTIONIPV6COUNT+$(grep -Enc ".*$LOGDATE.*connect from.*\[[0-9a-z]{0,4}\:.*" "$1")
+    let RELAYDENIED=$RELAYDENIED+$(grep -Enc ".*$LOGDATE.*NOQUEUE: reject.*Relay access denied.*" "$1")
+    let NOPTRCOUNT=$NOPTRCOUNT+$(grep -Enc ".*$LOGDATE.*NOQUEUE: reject.*cannot find your reverse hostname.*" "$1")
 
     # BLACKHOLE
-    let BLACKHOLEDMAILCOUNT=$BLACKHOLEDMAILCOUNT+$(egrep -nc ".*$LOGDATE.*Service unavailable.*blocked using.*" $1)
-
-    # SPAMASSASSIN
-    let TESTEDMAILCOUNT=$TESTEDMAILCOUNT+$(egrep -nc ".*$LOGDATE.*spamd: checking message.*" $1)
-    let SPAMCOUNT=$SPAMCOUNT+$(egrep -nc ".*$LOGDATE.*spamd: identified spam.*" $1)
-    let HAMCOUNT=$HAMCOUNT+$(egrep -nc ".*$LOGDATE.*spamd: clean message.*" $1)
+    let BLACKHOLEDMAILCOUNT=$BLACKHOLEDMAILCOUNT+$(grep -Enc ".*$LOGDATE.*Service unavailable.*blocked using.*" "$1")
 
     # OUTGOING
-    let SENTMAILCOUNT=$SENTMAILCOUNT+$(egrep -nc ".*$LOGDATE.*postfix/smtp.*status=sent.*" $1)
-    let BOUNCEDMAILCOUNT=$BOUNCEDMAILCOUNT+$(egrep -nc ".*$LOGDATE.*postfix/smtp.*status=bounced.*" $1)
+    let SENTMAILCOUNT=$SENTMAILCOUNT+$(grep -Enc ".*$LOGDATE.*postfix/smtp.*status=sent.*" "$1")
+    let BOUNCEDMAILCOUNT=$BOUNCEDMAILCOUNT+$(grep -Enc ".*$LOGDATE.*postfix/smtp.*status=bounced.*" "$1")
+
+    # SPAMASSASSIN
+    let TESTEDMAILCOUNT=$TESTEDMAILCOUNT+$(grep -Enc ".*$LOGDATE.*spamd: checking message.*" "$1")
+    let SPAMCOUNT=$SPAMCOUNT+$(grep -Enc ".*$LOGDATE.*spamd: identified spam.*" "$1")
+    let HAMCOUNT=$HAMCOUNT+$(grep -Enc ".*$LOGDATE.*spamd: clean message.*" "$1")
+
+    # RSPAMD
+    let RSPAMD_REJECTED=$RSPAMD_REJECTED+$(grep -Enc ".*$LOGDATE.* (reject):.*" "$1")
+    let RSPAMD_ADD_HEADER=$RSPAMD_ADD_HEADER+$(grep -Enc ".*$LOGDATE.* (add header):.*" "$1")
+    let RSPAMD_GREYLIST=$RSPAMD_GREYLIST+$(grep -Enc ".*$LOGDATE.* (greylist):.*" "$1")
+    let RSPAMD_NO_ACTION=$RSPAMD_NO_ACTION+$(grep -Enc ".*$LOGDATE.* (no action):.*" "$1")
+    let RSPAMD_SOFT_REJECT=$RSPAMD_SOFT_REJECT+$(grep -Enc ".*$LOGDATE.* (soft reject):.*" "$1")
+    let RSPAMD_REWRITE_SUBJECT=$RSPAMD_REWRITE_SUBJECT+$(grep -Enc ".*$LOGDATE.* (rewrite subject):.*" "$1")
 }
 
-for logfile in "${LOGFILES[@]}"
-do
-    echo "Reading Logfile ${logfile}..."
-    analyzeFile $logfile
+for logfile in "${LOGFILES[@]}"; do
+    echo "Analyzing Logfile ${logfile}..."
+    analyzeFile "$logfile"
     echo "Done."
 done
 
@@ -112,36 +128,40 @@ SPAMASSASSIN=$STOPPED
 CLAMAV=$STOPPED
 DOVECOT=$STOPPED
 OPENDKIM=$STOPPED
+RSPAMD=$STOPPED
 
-if (( $(ps -ef | grep -v grep | grep "postfix" | wc -l) > 0 ))
-then
+if (($(pgrep -c "smtpd") > 0)); then
     POSTFIX=$RUNNING
 fi
-if (( $(ps -ef | grep -v grep | grep "spamassassin" | wc -l) > 0 ))
-then
+if (($(pgrep -c "rspamd") > 0)); then
+    RSPAMD=$RUNNING
+fi
+if (($(pgrep -c "spamd") > 0)); then
     SPAMASSASSIN=$RUNNING
 fi
-if (( $(ps -ef | grep -v grep | grep "clamav" | wc -l) > 0 ))
-then
+if (($(pgrep -c "clamav") > 0)); then
     CLAMAV=$RUNNING
 fi
-if (( $(ps -ef | grep -v grep | grep "dovecot" | wc -l) > 0 ))
-then
+if (($(pgrep -c "dovecot") > 0)); then
     DOVECOT=$RUNNING
 fi
-if (( $(ps -ef | grep -v grep | grep "opendkim" | wc -l) > 0 ))
-then
+if (($(pgrep -c "opendkim") > 0)); then
     OPENDKIM=$RUNNING
 fi
 
-# Source E-Mail Template and send Mail
-source "$SCRIPTPATH/mailtemplate.sh"
+# load E-Mail Template from HTML file and replace all variables in the template
+TEMPLATE=$(eval "cat << EOF
+$(<mailtemplate.html)
+EOF
+")
 
-echo $body | mail \
--a "From: $MAILFROM" \
--a "MIME-Version: 1.0" \
--a "Content-Type: text/html" \
--s "$MAILSUBJECT" \
-"$MAILTO"
+# send E-Mail using the parameters from configuration
+
+echo "$TEMPLATE" | mail \
+    -a "From: $MAILFROM" \
+    -a "MIME-Version: 1.0" \
+    -a "Content-Type: text/html" \
+    -s "$MAILSUBJECT" \
+    "$MAILTO"
 
 exit 0
