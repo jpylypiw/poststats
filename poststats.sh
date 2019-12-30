8f1d4d5f7b694743fec9afaade254784ac69ebf8
jpylypiw <jochen@pylypiw.com>
2019-12-30 09:43:59 +0100

    switched from egrep to perl version

--- poststats.sh
+++ poststats.sh
@@ -70,50 +70,50 @@ RSPAMD_REWRITE_SUBJECT=0
 
 function analyzeFile() {
     # CLAMAV
-    let CLEANCOUNT=$CLEANCOUNT+$(grep -Enc ".*$LOGDATE.*Clean message from.*" "$1")
-    let INFECTEDCOUNT=$INFECTEDCOUNT+$(grep -Enc ".*$LOGDATE.*infected by.*" "$1")
+    let CLEANCOUNT=$CLEANCOUNT+$(grep -Pnc ".*$LOGDATE.*Clean message from.*" "$1")
+    let INFECTEDCOUNT=$INFECTEDCOUNT+$(grep -Pnc ".*$LOGDATE.*infected by.*" "$1")
 
     # IMAP
-    let IMAPLOGINCOUNT=$IMAPLOGINCOUNT+$(grep -Enc ".*$LOGDATE.*imap-login\: Login\: .*" "$1")
-    let IMAPLOGINFAILCOUNT=$IMAPLOGINFAILCOUNT+$(grep -Enc ".*$LOGDATE.*imap-login\: Disconnected.*" "$1")
-    let IMAPIPV4LOGINCOUNT=$IMAPIPV4LOGINCOUNT+$(grep -Enc ".*$LOGDATE.*imap-login\: Login\: .*rip=[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}.*" "$1")
-    let IMAPIPV6LOGINCOUNT=$IMAPIPV6LOGINCOUNT+$(grep -Enc ".*$LOGDATE.*imap-login\: Login\: .*rip=[0-9a-z]{0,4}\:[0-9a-z]{0,4}\:[0-9a-z]{1,4}.*" "$1")
-    let IMAPLOCALLOGINCOUNT=$IMAPLOCALLOGINCOUNT+$(grep -Enc ".*$LOGDATE.*imap-login\: Login\: .*rip=(?<rip>[\.\:0-9]{3,}).*lip=(?<lip>[\.\:0-9]+)?\k'rip'.*" "$1")
+    let IMAPLOGINCOUNT=$IMAPLOGINCOUNT+$(grep -Pnc ".*$LOGDATE.*imap-login\: Login\: .*" "$1")
+    let IMAPLOGINFAILCOUNT=$IMAPLOGINFAILCOUNT+$(grep -Pnc ".*$LOGDATE.*imap-login\: Disconnected.*" "$1")
+    let IMAPIPV4LOGINCOUNT=$IMAPIPV4LOGINCOUNT+$(grep -Pnc ".*$LOGDATE.*imap-login\: Login\: .*rip=[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}.*" "$1")
+    let IMAPIPV6LOGINCOUNT=$IMAPIPV6LOGINCOUNT+$(grep -Pnc ".*$LOGDATE.*imap-login\: Login\: .*rip=[0-9a-z]{0,4}\:[0-9a-z]{0,4}\:[0-9a-z]{1,4}.*" "$1")
+    let IMAPLOCALLOGINCOUNT=$IMAPLOCALLOGINCOUNT+$(grep -Pnc ".*$LOGDATE.*imap-login\: Login\: .*rip=(?<rip>[\.\:0-9]{3,}).*lip=(?<lip>[\.\:0-9]+)?\k'rip'.*" "$1")
 
     # POP3
-    let POP3LOGINCOUNT=$POP3LOGINCOUNT+$(grep -Enc ".*$LOGDATE.*pop3-login\: Login\: .*" "$1")
-    let POP3LOGINFAILCOUNT=$POP3LOGINFAILCOUNT+$(grep -Enc ".*$LOGDATE.*pop3-login: Disconnected.*" "$1")
-    let POP3IPV4LOGINCOUNT=$POP3IPV4LOGINCOUNT+$(grep -Enc ".*$LOGDATE.*pop3-login\: Login\: .*rip=[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}.*" "$1")
-    let POP3IPV6LOGINCOUNT=$POP3IPV6LOGINCOUNT+$(grep -Enc ".*$LOGDATE.*pop3-login\: Login\: .*rip=[0-9a-z]{0,4}\:[0-9a-z]{0,4}\:[0-9a-z]{1,4}.*" "$1")
-    let POP3LOCALLOGINCOUNT=$POP3LOCALLOGINCOUNT+$(grep -Enc ".*$LOGDATE.*pop3-login\: Login\: .*rip=(?<rip>[\.\:0-9]{3,}).*lip=(?<lip>[\.\:0-9]+)?\k'rip'.*" "$1")
+    let POP3LOGINCOUNT=$POP3LOGINCOUNT+$(grep -Pnc ".*$LOGDATE.*pop3-login\: Login\: .*" "$1")
+    let POP3LOGINFAILCOUNT=$POP3LOGINFAILCOUNT+$(grep -Pnc ".*$LOGDATE.*pop3-login: Disconnected.*" "$1")
+    let POP3IPV4LOGINCOUNT=$POP3IPV4LOGINCOUNT+$(grep -Pnc ".*$LOGDATE.*pop3-login\: Login\: .*rip=[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}.*" "$1")
+    let POP3IPV6LOGINCOUNT=$POP3IPV6LOGINCOUNT+$(grep -Pnc ".*$LOGDATE.*pop3-login\: Login\: .*rip=[0-9a-z]{0,4}\:[0-9a-z]{0,4}\:[0-9a-z]{1,4}.*" "$1")
+    let POP3LOCALLOGINCOUNT=$POP3LOCALLOGINCOUNT+$(grep -Pnc ".*$LOGDATE.*pop3-login\: Login\: .*rip=(?<rip>[\.\:0-9]{3,}).*lip=(?<lip>[\.\:0-9]+)?\k'rip'.*" "$1")
 
     # CONNECTION
-    let CONNECTIONCOUNT=$CONNECTIONCOUNT+$(grep -Enc ".*$LOGDATE.* connect from.*" "$1")
-    let CONNECTIONIPV4COUNT=$CONNECTIONIPV4COUNT+$(grep -Enc ".*$LOGDATE.* connect from.*[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}.*" "$1")
-    let CONNECTIONIPV6COUNT=$CONNECTIONIPV6COUNT+$(grep -Enc ".*$LOGDATE.* connect from.*[0-9a-z]{0,4}\:[0-9a-z]{0,4}\:[0-9a-z]{1,4}.*" "$1")
-    let RELAYDENIED=$RELAYDENIED+$(grep -Enc ".*$LOGDATE.*NOQUEUE: reject.*Relay access denied.*" "$1")
-    let NOPTRCOUNT=$NOPTRCOUNT+$(grep -Enc ".*$LOGDATE.*NOQUEUE: reject.*cannot find your reverse hostname.*" "$1")
-    let NODOMAINCOUNT=$NODOMAINCOUNT+$(grep -Enc ".*$LOGDATE.*NOQUEUE: reject.*cannot find your reverse hostname.*" "$1")
+    let CONNECTIONCOUNT=$CONNECTIONCOUNT+$(grep -Pnc ".*$LOGDATE.* connect from.*" "$1")
+    let CONNECTIONIPV4COUNT=$CONNECTIONIPV4COUNT+$(grep -Pnc ".*$LOGDATE.* connect from.*[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}.*" "$1")
+    let CONNECTIONIPV6COUNT=$CONNECTIONIPV6COUNT+$(grep -Pnc ".*$LOGDATE.* connect from.*[0-9a-z]{0,4}\:[0-9a-z]{0,4}\:[0-9a-z]{1,4}.*" "$1")
+    let RELAYDENIED=$RELAYDENIED+$(grep -Pnc ".*$LOGDATE.*NOQUEUE: reject.*Relay access denied.*" "$1")
+    let NOPTRCOUNT=$NOPTRCOUNT+$(grep -Pnc ".*$LOGDATE.*NOQUEUE: reject.*cannot find your reverse hostname.*" "$1")
+    let NODOMAINCOUNT=$NODOMAINCOUNT+$(grep -Pnc ".*$LOGDATE.*NOQUEUE: reject.*cannot find your reverse hostname.*" "$1")
 
     # BLACKHOLE
-    let BLACKHOLEDMAILCOUNT=$BLACKHOLEDMAILCOUNT+$(grep -Enc ".*$LOGDATE.*Service unavailable.*blocked using.*" "$1")
+    let BLACKHOLEDMAILCOUNT=$BLACKHOLEDMAILCOUNT+$(grep -Pnc ".*$LOGDATE.*Service unavailable.*blocked using.*" "$1")
 
     # OUTGOING
     let SENTMAILCOUNT=$SENTMAILCOUNT+$(grep -Pnc ".*$LOGDATE.*relay=(?=.*\[[0-9\.\:A-z]*\]).*status=sent.*" "$1")
     let BOUNCEDMAILCOUNT=$BOUNCEDMAILCOUNT+$(grep -Pnc ".*$LOGDATE.*relay=(?=.*\[[0-9\.\:A-z]*\]).*status=bounced.*" "$1")
 
     # SPAMASSASSIN
-    let TESTEDMAILCOUNT=$TESTEDMAILCOUNT+$(grep -Enc ".*$LOGDATE.*spamd: checking message.*" "$1")
-    let SPAMCOUNT=$SPAMCOUNT+$(grep -Enc ".*$LOGDATE.*spamd: identified spam.*" "$1")
-    let HAMCOUNT=$HAMCOUNT+$(grep -Enc ".*$LOGDATE.*spamd: clean message.*" "$1")
+    let TESTEDMAILCOUNT=$TESTEDMAILCOUNT+$(grep -Pnc ".*$LOGDATE.*spamd: checking message.*" "$1")
+    let SPAMCOUNT=$SPAMCOUNT+$(grep -Pnc ".*$LOGDATE.*spamd: identified spam.*" "$1")
+    let HAMCOUNT=$HAMCOUNT+$(grep -Pnc ".*$LOGDATE.*spamd: clean message.*" "$1")
 
     # RSPAMD
-    let RSPAMD_REJECTED=$RSPAMD_REJECTED+$(grep -Enc "$LOGDATERSPAMD.*default\: . \(reject\)\:.*" "$1")
-    let RSPAMD_ADD_HEADER=$RSPAMD_ADD_HEADER+$(grep -Enc "$LOGDATERSPAMD.*default\: . \(add header)\:.*" "$1")
-    let RSPAMD_GREYLIST=$RSPAMD_GREYLIST+$(grep -Enc "$LOGDATERSPAMD.*default\: . \(greylist)\:.*" "$1")
-    let RSPAMD_NO_ACTION=$RSPAMD_NO_ACTION+$(grep -Enc "$LOGDATERSPAMD.*default\: . \(no action)\:.*" "$1")
-    let RSPAMD_SOFT_REJECT=$RSPAMD_SOFT_REJECT+$(grep -Enc "$LOGDATERSPAMD.*default\: . \(soft reject)\:.*" "$1")
-    let RSPAMD_REWRITE_SUBJECT=$RSPAMD_REWRITE_SUBJECT+$(grep -Enc "$LOGDATERSPAMD.*default\: . \(rewrite subject)\:.*" "$1")
+    let RSPAMD_REJECTED=$RSPAMD_REJECTED+$(grep -Pnc "$LOGDATERSPAMD.*default\: . \(reject\)\:.*" "$1")
+    let RSPAMD_ADD_HEADER=$RSPAMD_ADD_HEADER+$(grep -Pnc "$LOGDATERSPAMD.*default\: . \(add header)\:.*" "$1")
+    let RSPAMD_GREYLIST=$RSPAMD_GREYLIST+$(grep -Pnc "$LOGDATERSPAMD.*default\: . \(greylist)\:.*" "$1")
+    let RSPAMD_NO_ACTION=$RSPAMD_NO_ACTION+$(grep -Pnc "$LOGDATERSPAMD.*default\: . \(no action)\:.*" "$1")
+    let RSPAMD_SOFT_REJECT=$RSPAMD_SOFT_REJECT+$(grep -Pnc "$LOGDATERSPAMD.*default\: . \(soft reject)\:.*" "$1")
+    let RSPAMD_REWRITE_SUBJECT=$RSPAMD_REWRITE_SUBJECT+$(grep -Pnc "$LOGDATERSPAMD.*default\: . \(rewrite subject)\:.*" "$1")
 }
 
 for logfile in "${LOGFILES[@]}"; do
